97f3bec4a64efbd78afcee6f142c6aec3e5de2d3
NAPHATSAWAN <naphatsawan.bw@mail.kmutt.ac.th>
2020-10-20 15:35:22 +0700

    First commit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,15 @@
+
+package gittest;
+
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Test();
+    }
+    public static void Test() {
+        System.out.println("HELLO");
+    }
+}
